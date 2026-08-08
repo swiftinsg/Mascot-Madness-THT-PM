8853db38a396f09ac5200025c0226cd9fbd2e586
Rui Ting <rui.tingggg.2@gmail.com>
2026-08-08 15:21:59 +0800

    7pm left arm

--- MascotMadness/Arms/LeftArmView.swift
+++ MascotMadness/Arms/LeftArmView.swift
@@ -4,21 +4,20 @@ var leftArm: some BodyPart {
     LeftArm {
         LeftUpperArmSegment {
             ZStack {
-                Rectangle()
-                    .frame(width: 30, height: 200)
-                    .foregroundStyle(.orange)
+                
+                Image(.lala)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400, height: 400)
             }
         }
     } foreArm: {
         LeftLowerArmSegment {
-            ZStack {
-                Rectangle()
-                    .frame(width: 30, height: 200)
-                    .foregroundStyle(.red)
+            
             }
         }
     }
-}
+
 
 #Preview(traits: .fixedLayout(width: 200, height: 400)) {
     leftArm
